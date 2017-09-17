c761f23c375c946cf497b8fa00af27af8f6d0790
vokythoai <vokythoai@gmail.com>
2017-09-17 14:14:51 +0700

    Update assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,6 +19,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add folder with webpack generated assets to assets.paths
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
+Rails.application.config.assets.paths << Rails.root.join('/app/assets/fonts')
 Rails.application.config.assets.paths << Rails.root.join('app', 'vendor', 'assets', 'stylesheets', 'css', 'fonts')
 Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
 Rails.application.config.assets.precompile << Proc.new { |path, fn| fn =~ /vendor\/assets\/images/ }
