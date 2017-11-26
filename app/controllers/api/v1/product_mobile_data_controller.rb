9f7ec96af6c646cde2544f1ba2fa8ce579f75d3a
vokythoai <vokythoai@gmail.com>
2017-11-26 19:05:18 +0700

    Update search similar product

--- app/controllers/api/v1/product_mobile_data_controller.rb
+++ app/controllers/api/v1/product_mobile_data_controller.rb
@@ -7,14 +7,26 @@ module Api
       end
 
       def show
-        render json: ProductMobileData.all.limit(10)
+        render json: ProductMobileData.all.limit(10).group_by("product_title").group_by("page_source")
       end
 
       def search
-        search_string = "%" + params["search"].split("").join("%") + "%"
-        search_result = ProductMobileData.where("product_title ILIKE ?", search_string)
+        search_result = ProductMobileData.where("product_title ILIKE ?", custom_search_params(params)).group_by("page_source")
         render json: search_result
       end
+
+      def similar_product
+        original_page_source = params["page_source"]
+        similar_product_result = ProductMobileData
+                                .where("product_title ILIKE :search_params AND page_source != :page_source", { search_params: custom_search_params(params), page_source: original_page_source })
+                                .where("created_at >= ?", Time.zone.now.beginning_of_day)
+        render json: similar_product_result
+      end
+
+      private
+        def custom_search_params search_params
+          search_string = "%" + search_params["search"].split("").join("%") + "%"
+        end
     end
   end
 end
